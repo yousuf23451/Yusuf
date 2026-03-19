86f378a2339d4bc1df7558cb4235f6bb716983e5
yousuf23451 <jwna85068@gmail.com>
2026-03-18 20:06:30 -0700

    Delete event data from data.js
    Removed event data related to global conflicts and peace talks.

--- data.js
+++ data.js
@@ -1,7 +1,1 @@
-const events = [
-  { title: "Missile attack reported in Gaza", lat:31.5, lng:34.5, type:"war" },
-  { title: "Peace talks between two countries", lat:51.5, lng:0.1, type:"peace" },
-  { title: "Military tensions rising in Ukraine", lat:50.4, lng:30.5, type:"war" },
-  { title: "New defense agreement signed", lat:38.9, lng:-77.0, type:"peace" },
-  { title: "War threats increasing globally", lat:35.7, lng:139.7, type:"alert" }
-];
+
